9efab2f6fa97f5924449fbfb7dc4d6bdeadf8443
Reloashed <remscherr@gmail.com>
2025-04-07 15:28:49 +0200

    fix error

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Albums from "./components/data";
 export default function Home() {
     return (
         <>
-            <H  eader />
+            <Header />
             <div className="flex flex-col items-center justify-center p-8 bg-gray-100">
                 <h1 className="text-4xl font-bold text-blue-600 mb-4">Willkommen auf der Musik App</h1>
                 <p className="text-lg text-gray-700 mb-6">Entdecke deine Lieblingsmusik und bleibe immer up-to-date.</p>
